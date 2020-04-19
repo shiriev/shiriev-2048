fed6b8182dc85eab134de9d1dffa963f70a4ed01
Шириев Рустем Венерович <Shiriev.RV@gazprom-neft.ru>
2020-04-19 18:41:33 +0500

    deploy

--- README.md
+++
@@ -1,2 +0,0 @@
-# shiriev-2048
-2048 game. Pet project for learning React.
---
+++ precache-manifest.c19de4699d287b46baa4d5283a792123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc87e3fdff21485d7c24a1678c9213a4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3471523262ad0e81e2b",
+    "url": "./static/css/main.87299a14.chunk.css"
+  },
+  {
+    "revision": "a8dbfa231eef3dd0f896",
+    "url": "./static/js/2.2f2cd127.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.2f2cd127.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3471523262ad0e81e2b",
+    "url": "./static/js/main.a447d1b8.chunk.js"
+  },
+  {
+    "revision": "2266b1530d4ec5499287",
+    "url": "./static/js/runtime-main.d3411ad5.js"
+  }
+]);
