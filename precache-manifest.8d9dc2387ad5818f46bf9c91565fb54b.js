044d08ff6c2ee0a82cc7da0660f5a54174ee20c1
Шириев Рустем Венерович <Shiriev.RV@gazprom-neft.ru>
2020-04-22 16:29:43 +0500

    Updates

--- precache-manifest.8d9dc2387ad5818f46bf9c91565fb54b.js
+++ precache-manifest.8d9dc2387ad5818f46bf9c91565fb54b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d3057861fa7a51591811b94c406823d",
+    "revision": "bdea90306e3e2000ba115903cb038fd9",
     "url": "./index.html"
   },
   {
-    "revision": "a194aee3027ab08d1d00",
-    "url": "./static/css/main.b190d3ad.chunk.css"
+    "revision": "c10fd2537b62d603cc9c",
+    "url": "./static/css/main.60d6c8d6.chunk.css"
   },
   {
     "revision": "a8dbfa231eef3dd0f896",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.2f2cd127.chunk.js.LICENSE"
   },
   {
-    "revision": "a194aee3027ab08d1d00",
+    "revision": "c10fd2537b62d603cc9c",
     "url": "./static/js/main.7db54626.chunk.js"
   },
   {
